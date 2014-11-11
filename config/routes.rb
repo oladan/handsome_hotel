fc92d659e86173b744f1c1e0a00ed7432681eefa
oladan <daniel@oladan.com>
2014-11-11 19:16:01 +0300

    Add a Statis Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 HandsomeHotel::Application.routes.draw do
+  get "static_pages/help"
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
