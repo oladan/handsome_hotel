9015ef8960b078284340bdfdc74fa0ed52ad9c4d
oladan <daniel@oladan.com>
2014-11-13 21:18:09 +0300

    Simple hotel model finished

--- spec/controllers/hotels_controller_spec.rb
+++ spec/controllers/hotels_controller_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe HotelsController do
+  let(:user) { create(:user) }
   before do
-    @user = FactoryGirl.create :user
-    sign_in @user
+    sign_in user
   end
 
   describe "GET 'new'" do
@@ -13,17 +13,36 @@ describe HotelsController do
     end
   end
 
-  describe "GET 'create'" do
-    it "returns http success" do
-      get 'create'
-      response.should be_success
+  describe "POST 'create'" do
+    context "with valid attributes" do 
+      it "creates a new hotel" do 
+        expect{ 
+          post :create, hotel: FactoryGirl.attributes_for(:hotel) 
+        }.to change(Hotel,:count).by(1) 
+      end 
+      it "redirects to the new hotel" do 
+        post :create, hotel: FactoryGirl.attributes_for(:hotel) 
+        response.should redirect_to Hotel.last 
+      end 
     end
+    context "with invalid attributes" do 
+      it "does not save the new hotel" do 
+        expect{ 
+          post :create, hotel: FactoryGirl.attributes_for(:invalid_hotel) 
+        }.to_not change(Hotel,:count) 
+      end 
+      it "re-renders the new method" do 
+        post :create, hotel: FactoryGirl.attributes_for(:invalid_hotel) 
+        response.should render_template :new 
+      end 
+    end 
   end
 
   describe "GET 'show'" do
-    it "returns http success" do
-      get 'show'
-      response.should be_success
+    it "assigns the requested hotel to @hotel" do
+      hotel = create(:hotel)
+      get :show, id: hotel
+      assigns(:hotel).should eq(hotel)
     end
   end
 
--- spec/factories/hotel.rb
+++ spec/factories/hotel.rb
@@ -1,7 +1,13 @@
 FactoryGirl.define do
+
   factory :hotel do
     title "My first hotel"
     description "Sample hotel description"
     breakfast true
   end
+
+  factory :invalid_hotel, parent: :hotel do
+    title nil
+  end
+
 end
